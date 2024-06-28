57d08402e0e3e02653d5aedf85963029990f213f
wbuz24 <wbuziak@vols.utk.edu>
2024-06-28 16:06:35 -0400

    Saving scheme

--- setup.sh
+++ setup.sh
@@ -2,26 +2,48 @@
 
 # Check the existence of an ".old_dotfiles" directory
 if [ -d ~/.old_dotfiles ]; then
-	echo 'Directory: bin/ exists' 1> /dev/null
+	echo 'Directory: ~/.old_dotfiles/ exists'
 else
 	mkdir ~/.old_dotfiles 1> /dev/null
+  echo "Created ~/.old_dotfiles"
 
 fi
-echo "Created ~/.old_dotfiles"
+echo ""
 
 # Search for the old dotfiles and move them to a folder
 
+if [ -f ~/.old_dotfiles/bashrc ]; then
+  current_date=$(date +"%Y-%m-%d")
+  file=~/.old_dotfiles/bashrc-$current_date
+  if [ -f $file ]; then
+    rm $file
+  fi
+  
+  mv ~/.old_dotfiles/bashrc $file
+  echo 'Copied and dated the existing bashrc file'
+fi
+
+if [ -f ~/.old_dotfiles/vimrc ]; then
+  current_date=$(date +"%Y-%m-%d")
+  file=~/.old_dotfiles/vimrc-$current_date
+  if [ -f $file ]; then
+    rm $file
+  fi
+  mv ~/.old_dotfiles/vimrc $file
+  echo 'Copied and dated the existing vimrc file'
+fi
+
+
 if [ -f ~/.bashrc ]; then 
-	echo ""
 	mv ~/.bashrc ~/.old_dotfiles/bashrc
-	echo "Moved .bashrc ~/.old_dotfiles"
+	echo "Moved .bashrc to ~/.old_dotfiles/bashrc"
 fi
 
 if [ -f ~/.vimrc ]; then
-	echo ""
 	mv ~/.vimrc ~/.old_dotfiles/vimrc
-	echo "Moved .vimrc ~/.old_dotfiles"
+	echo "Moved .vimrc to ~/.old_dotfiles/vimrc"
 fi
+echo ""
 
 
 if [ -f bashrc ]; then
@@ -31,7 +53,6 @@ if [ -f vimrc ]; then
 	ln ./vimrc ~/.vimrc
 fi
 
-echo ""
 echo "Links created"
 echo ""
 
